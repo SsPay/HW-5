48a6d946d678ec1c531c6dcb0776f77ef2a2d533
SsPay <truesspay@gmail.com>
2019-11-15 22:37:51 +0200

    added model Author

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_10_135538) do
+ActiveRecord::Schema.define(version: 2019_11_15_195624) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "gender"
+    t.date "birthday"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "name"
