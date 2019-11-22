8e64c9bc67f3613afd2e8f2a2cdd85c14e9b6e49
SsPay <truesspay@gmail.com>
2019-11-22 22:33:12 +0200

    AAAAA SYKA VTOROI RAZ COMICHU NI4E NE SOHRANILOS', PRISHLOS' ZANOVO VSE DELAT' AAAAAAAA BAMBIT SESIU DOBAVIL I VALIDACII 4UT'-4UT'

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,4 +3,11 @@ class Author < ApplicationRecord
   has_many :posts
   validates :email, presence: true, uniqueness: true
   validates :login, presence: true, uniqueness: true
+  validate :email_val
+  private
+  def email_val
+    unless email.include?('@')
+      errors.add(:email, "invalid")
+    end
+  end
 end
