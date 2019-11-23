23cc470a97e8ca4210ba44c8983130b421697d44
SsPay <truesspay@gmail.com>
2019-11-23 19:31:21 +0200

    fixed comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,18 +2,37 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
-    redirect_to post_path(@post)
+    @comment.author_id = current_author.id
+    respond_to do |format|
+      #dev, del later
+      # ##############
+    if @comment.save
+      format.html { redirect_to @post, notice: 'good' }
+    else
+      format.html { redirect_to @post, notice: 'bad' }
+      end
+    end
   end
 
   def destroy
     @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
+    if (@current_author.id == @comment.author_id)
     @comment.destroy
     redirect_to post_path(@post)
+      #add mess
+      # #############
+    else
+      redirect_to root_path
+      #add smth instead
+      # #############
+    end
+
   end
+
   private
 
   def comment_params
-    params.require(:comment).permit(:username, :body)
+    params.require(:comment).permit(:body, :author_id, :user)
   end
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,7 @@
 class Author < ApplicationRecord
   has_secure_password
-  has_many :posts
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
   validates :email, presence: true, uniqueness: true
   validates :login, presence: true, uniqueness: true
   validate :email_val
