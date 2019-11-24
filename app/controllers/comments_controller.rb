0238229c166669384b0ed067d2fe3ddd1c78e39d
SsPay <truesspay@gmail.com>
2019-11-24 18:21:46 +0200

    add some beautiful

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -64,6 +64,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:body, :author_id, :user)
+    params.require(:comment).permit(:body, :author_id)
   end
 end
