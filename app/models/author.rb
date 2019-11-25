01ec46e5b13516348f6503e2882ad6103a9b8da9
SsPay <truesspay@gmail.com>
2019-11-25 19:00:20 +0200

    add password validation

--- app/models/author.rb
+++ app/models/author.rb
@@ -4,11 +4,17 @@ class Author < ApplicationRecord
   has_many :comments, dependent: :destroy
   validates :email, presence: true, uniqueness: true
   validates :login, presence: true, uniqueness: true
-  validate :email_val
+  validate :email_val, :pass_val
   private
   def email_val
     unless email.include?('@')
       errors.add(:email, "invalid")
+    end
+  end
+
+  def pass_val
+    unless  password.count("a-z") > 0 && password.count("A-Z") > 0 && password.count((0-9).to_s) > 0
+      errors.add(:password, "must contain 1 small letter, 1 capital letter and number")
     end
   end
 end
