8565e5c0fe998d01f5072ebdd0e9b4e5e8bb7558
SsPay <truesspay@gmail.com>
2019-12-10 16:15:00 +0200

    add masterkey

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,8 +48,8 @@ Rails.application.configure do
  address:              'smtp.gmail.com',
  port:                 587,
  domain:               'gmail.com',
- user_name:            'truesspay@gmail.com',
- password:             'canwqfvujhormvpw',
+ user_name:            Rails.application.credentials[:gmail_user_name],
+ password:             Rails.application.credentials[:gmail_password],
  authentication:       'plain',
  enable_starttls_auto: true }
  config.cache_classes = false
